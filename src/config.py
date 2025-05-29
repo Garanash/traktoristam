ebd056a9c11488ba0f25fd0437c6e2fa081ae959
a.dolgov <dolgov_am@mail.ru>
2025-05-29 10:05:26 +0300

    v8 super stable vers, with comments

--- src/config.py
+++ src/config.py
@@ -1,15 +1,14 @@
 # Конфигурационные данные
-API_ID = 23715784                # Замените на ваш API ID
-API_HASH = "fb3627e3100eec5a09121035d462338a"       # Замените на ваш API HASH
+API_ID = 23715784
+API_HASH = "fb3627e3100eec5a09121035d462338a" # Замените на ваш API HASH
 PHONE_NUMBER = "+79152048909"
 
 
-BOT_TOKEN = "7992422589:AAEKoEtmgWFjQVnDmaMIYdFLrINKK8IFmmM"     # Токен бота от @BotFather
-PERPLEXITY_API_KEY = "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"
+BOT_TOKEN = "7992422589:AAEKoEtmgWFjQVnDmaMIYdFLrINKK8IFmmM" # Токен бота от @BotFather
+PERPLEXITY_API_KEY = "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" # ключ ppxt
 
 CHANNEL_ID = '@k_zap'   # ID канала (например, -1001234567890)
-USER_ID = "@Garanash" # 476409056    #"@Garanash"            # ID пользователя, которому отправлять посты
+USER_ID = "@Garanash" # 476409056 #"@Garanash" # ID пользователя, которому отправлять посты
 
-OUTPUT_CHANNEL_ID = "@searcher_for_tractors_bot"  # Добавьте этот параметр
-
-PRIVATE_CHANNEL_ID = -1002508514704
+OUTPUT_CHANNEL_ID = "@searcher_for_tractors_bot" # сюда падают расцененные артикулы
+PRIVATE_CHANNEL_ID = -1002508514704 # здесь происходит расценка
